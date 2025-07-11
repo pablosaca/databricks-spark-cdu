93b6a9b98d3fc335056391c19254f4ae8b9d261e
p_san <psancabrera@gmail.com>
2025-07-11 21:23:41 +0200

    - actualización load.py

---
+++ src/data/load.py
@@ -0,0 +1,71 @@
+from typing import Tuple, Union, Optional
+from pyspark.sql.session import SparkSession
+from pyspark.sql import DataFrame as DF
+
+from src.utils.logger import get_logger
+
+
+logger = get_logger()
+
+
+class LoadData:
+    """
+    Clase que obtiene los tablones necesarios para realizar el entrenamiento y predicción del modelo
+    """
+
+    def __init__(
+            self,
+            spark_session: SparkSession,
+            profile_table_name: str,
+            performance_table_name: str,
+            socioeconomic_table_name: str,
+            target_table_name: Optional[str] = None
+    ):
+
+        self.spark_session = spark_session
+        self.profile_table_name = profile_table_name
+        self.performance_table_name = performance_table_name
+        self.socioeconomic_table_name = socioeconomic_table_name
+        self.target_table_name = target_table_name
+
+    def get_basic_data(self) -> Union[DF, Tuple[DF, DF]]:
+        """
+        Obtención de tablón de features (y target asociado si aplica)
+        En este método se cargan los ficheros y se genera el tablón para continuar
+
+        # TODO: ESTE MÉTODO DEBE SER USADO TANTO PARA OBTENER LA MUESTRA QUE SE USE PARA CREAR EL MODELO COMO PARA REALIZAR PREDICCIONES
+        """
+        # TODO: BORRAR A FUTURO TODO ESTE MÉTODO
+        profile_table_name_df = self.__load_data(self.profile_table_name)
+        performance_table_name_df = self.__load_data(self.performance_table_name)
+        socioeconomic_table_name_df = self.__load_data(self.socioeconomic_table_name)
+
+        df = self.__merge_tables(
+            profile_table_name_df, performance_table_name_df, "id"
+        )
+        df = self.__merge_tables(
+            df, socioeconomic_table_name_df, "Region_Code"
+        )
+        logger.info("Obtención del tablón de features")
+        if self.target_table_name is not None:
+            target_df = self.__load_data(self.target_table_name)
+            df = self.__merge_tables(df, target_df, "id")
+            logger.info("Obtención de tablón con target")
+        return df
+
+    def __load_data(self, table_name: str) -> DF:
+        """
+        Carga tabla de la database de databricks como Dataframe de spark
+        """
+        logger.info(f"Se carga el fichero {table_name}")
+        return self.spark_session.read.table(table_name)
+
+    @staticmethod
+    def __merge_tables(df1: DF, df2: DF, cols_merge: Union[str, list]) -> DF:
+        """
+        Cruce de tablas para construir el macrotablón
+        # TODO: DEFINE UNA FUNCIÓN PARA CRUZAR TABLAS EN PYSPARK
+        """
+        logger.info(f"Se cruzan dos tablas (left-join) por {cols_merge}")
+        df1 = df1.join(df2, on=cols_merge, how="left")
+        return df1
