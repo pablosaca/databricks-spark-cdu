355fe3b49dfbbad9b2ed06292831729b5ddbd43e
p_san <psancabrera@gmail.com>
2025-07-13 10:57:30 +0200

    actualización de la función `impute_nulls_for_numerical_cols_out_sample` en preprocessing.py

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -85,17 +85,22 @@ def impute_nulls_for_numerical_cols_out_sample(
             col_name,
             F.when(F.col(col_name).isNull(), impute_value_or_mapping).otherwise(F.col(col_name))
         )
+
     # asumimos que es un diccionario anidado con valores float o enteros
     elif isinstance(impute_value_or_mapping, dict):
         logger.info(f"imputando {col_name} usando {stratific_col}")
         mapping = impute_value_or_mapping.get(col_name, {})
+        if not mapping:
+            msg = f"Diccionario está vacío: {mapping}"
+            logger.error(msg)
+            raise ValueError(msg)
+        # uso de un bucle para encadenar las condiciones (en cascada)
+        expr = F.col(col_name)  # inicialización de la expresión con la condición inicial
         for cond_val, impute_val in mapping.items():
-            df = df.withColumn(
-                col_name,
-                F.when(
-                    (F.col(stratific_col) == cond_val) & (F.col(col_name).isNull()), impute_val
-                ).otherwise(F.col(col_name))
-            )
+            expr = F.when(
+                (F.col(stratific_col) == cond_val) & F.col(col_name).isNull(), impute_val
+            ).otherwise(expr)
+        df = df.withColumn(col_name, expr)
     logger.info(f"{col_name} ha sido imputada según {impute_value_or_mapping}")
     return df
 
